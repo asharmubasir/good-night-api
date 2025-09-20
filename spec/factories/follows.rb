c48b738dc35a2a03540ccfca4819c43e7a0f8329
Ashar Mubasir <asharmubasir@gmail.com>
2025-09-20 15:41:30 +0700

    setup follow

---
+++ spec/factories/follows.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: follows
+#
+#  id          :bigint           not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  followee_id :bigint           not null
+#  follower_id :bigint           not null
+#
+# Indexes
+#
+#  index_follows_on_followee_id                  (followee_id)
+#  index_follows_on_follower_id                  (follower_id)
+#  index_follows_on_follower_id_and_followee_id  (follower_id,followee_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (followee_id => users.id)
+#  fk_rails_...  (follower_id => users.id)
+#
+FactoryBot.define do
+  factory :follow do
+    
+  end
+end
