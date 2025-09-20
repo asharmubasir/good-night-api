e825f923364a84cf7cf29409f1270e5cd934b230
Ashar Mubasir <asharmubasir@gmail.com>
2025-09-20 10:12:27 +0700

    setup sleep records spec

--- spec/factories/sleep_records.rb
+++ spec/factories/sleep_records.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :sleep_record do
-    user { nil }
+    association :user
     slept_at { "2025-09-20 10:01:45" }
-    woke_up_at { "2025-09-20 10:01:45" }
+    woke_up_at { "2025-09-20 10:02:45" }
     duration_in_minutes { 1 }
   end
 end
